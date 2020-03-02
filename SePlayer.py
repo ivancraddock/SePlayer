9f4d68c96574c2cbb084c65b5755703e79e7bf81
ivan c <ivancraddock@gmail.com>
2020-03-01 21:46:46 -0800

    quad screen functionality working
    TODO: Remove menu bar from webdrivers running in -kiosk mode

--- SePlayer.py
+++ SePlayer.py
@@ -3,19 +3,33 @@ from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 from time import sleep
 
+X_SIZEMOD = 1
+X_OFFSET = 0
+Y_SIZEMOD = 35
+Y_OFFSET = -10
 
 '''This method shall an int (window) as args'''
 def instanceReader(window):
+    print(window)
     if os.path.isfile(f".cache/SEPLAYER_{window}") == False:
         return "file:///fff"
     with open(f'.cache/SEPLAYER_{window}', 'r') as f:
         file_name = f.read()
         return file_name
 
-def resolutionParser(window):
-    resolution_list = []
-    return resolution_list
-
+def resolutionParser(window,resolutions):
+    x_size = int(resolutions["width"]) // 2 - X_SIZEMOD
+    y_size = int(resolutions["height"]) // 2 - Y_SIZEMOD
+    x_pos = x_size - X_OFFSET
+    y_pos = y_size - Y_OFFSET
+    
+    if window == 2:
+        return [x_pos,0,x_size,y_size]
+    if window == 3:
+        return [0,y_pos,x_size,y_size]
+    if window == 4:
+        return [x_pos,y_pos,x_size,y_size]
+    return [0,0,x_size,y_size]
 
 '''
 This method shall take an int (window) as args.
@@ -30,7 +44,7 @@ This method shall take an int (window) as args.
 '''
 def driverLauncher(window):
     
-    resolution_list = resolutionParser(window)
+
     file_name = instanceReader(window)
     print(file_name)
     #start webdriver of correct size/position
@@ -40,10 +54,12 @@ def driverLauncher(window):
     first_pass = True
 
     with Firefox(options=opts) as driver:
+        resolutions = driver.get_window_size()
+        parsed_resolutions = resolutionParser(window,resolutions)
         driver.get(file_name)
         if first_pass:
-            driver.set_window_position(100,100)
-
+            driver.set_window_position(parsed_resolutions[0],parsed_resolutions[1])
+            driver.set_window_size(parsed_resolutions[2],parsed_resolutions[3])
             first_pass = False
 
         while(True):
@@ -58,7 +74,11 @@ def driverLauncher(window):
 
 
 if __name__ == "__main__":
-    window = int(sys.argv[1])
+    window = 1
+    for i in sys.argv[1:]:
+        if "-w=" in i[:3]:
+            window = int(i[3])
+
 
     driverLauncher(window)
     
